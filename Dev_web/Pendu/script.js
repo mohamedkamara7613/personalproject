258eeda3035c71a96fe8f4aaccd23c4f9d4d1d46
mohamedkamara7613 <mohamedkamara7613@gmail.com>
2025-01-01 09:06:43 +0100

    `Added restart functionality and implemented game logic in Pendu game`

--- Dev_web/Pendu/script.js
+++ Dev_web/Pendu/script.js
@@ -6,11 +6,43 @@ fetch("liste.txt")
     var randomIndex = Math.floor(Math.random() * wordList.length); //Comment fonctionne Math.floor, Math.range()
     const wordToGuess = wordList[randomIndex];
 
+    alert(wordToGuess);
+
     var wordLength = wordToGuess.length;
     var hiddenWord = "";
-    for(i=0; i < wordLength; i++){
-        hiddenWord += "_"
+    for(var i=0; i < wordLength; i++){
+        hiddenWord += "_";
     }
 
-    document.getElementById("word").innerHTML = hiddenWord 
+    document.getElementById("word").innerHTML = hiddenWord;
+    var guessInput = document.getElementById("guess");
+    var submitButton = document.getElementById("submit");
+    var link = document.getElementById("link"); 
+    var result = document.getElementById("result") ; 
+
+    submitButton.onclick = function(){
+        var guess = guessInput.value;
+
+        if(guess.length > 1 || guess.length === 0){
+            result.innerHTML = "Entrer une seule lettre !";
+        }else if(wordToGuess.indexOf(guess) === -1){
+            result.innerHTML = "Mauvaise lettre !";
+        }else{
+            for(var i = 0; i < wordToGuess.length; i++){
+                if(wordToGuess[i] === guess){
+                    hiddenWord = hiddenWord.substr(0,i) + guess + hiddenWord.substr(i+1);
+                }
+            }
+
+            document.getElementById("word").innerHTML = hiddenWord;
+
+            if(hiddenWord === wordToGuess){
+                result.innerHTML = "Bravo !";
+                guessInput.style.display = "none";
+                submitButton.style.display = "none";
+                link.style.display = "block";
+
+            }
+        }
+    }
 })
