8253ff5548ba3dd5f3f85fdccbcf7c547f17482e
mohamedkamara7613 <mohamedkamara7613@gmail.com>
2025-01-03 17:18:02 +0100

    `Refactor quicksort implementation in quicsort.ml`

--- Autres/Tri/Quicksort/quicsort.ml
+++ Autres/Tri/Quicksort/quicsort.ml
@@ -15,11 +15,11 @@
 
 
 
-
+(*Code qui ne marche pas *)
 let rec partitionner liste pivot less more = match liste with
-  | [] -> (less, more)
+  | [] -> less, more
   | t :: reste ->
-      if t < pivot then (partitionner reste pivot ([t]::less) more)
+      if t <= pivot then (partitionner reste pivot ([t]::less) more)
       else (partitionner reste pivot less ([t]::more))
 ;;
 
@@ -27,13 +27,26 @@ let rec partitionner liste pivot less more = match liste with
 let rec quicksort liste = 
   match liste with
   | [] -> []
-  | [a] -> [a]
   | pivot :: reste ->
       let plus_petit, plus_grand = partitionner liste pivot [] [] in
       quicksort plus_petit @ [pivot] @ quicksort plus_grand
 ;;
 
+(* Code qui marche*)
+let rec partition pivot less more = function
+  | [] -> less, more
+  | x :: rest ->
+      if x <= pivot then
+        partition pivot (x :: less) more rest
+      else
+        partition pivot less (x :: more) rest
 
+let rec quicksort = function
+  | [] -> []
+  | pivot :: rest ->
+      let less, more = partition pivot [] [] rest in
+      quicksort less @ [pivot] @ quicksort more
+      
 let l = [1;2;8;6];;
 
 quicksort l;;
