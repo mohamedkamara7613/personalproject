56d43f90641d9eb48f5d6d248454709ffebc3f8c
mohamedkamara7613 <mohamedkamara7613@gmail.com>
2025-01-07 21:34:20 +0100

    `Added BOX_SIZE constant and grid management to SnakeGame class`

--- Autres/Python/Snake_Game/main.py
+++ Autres/Python/Snake_Game/main.py
@@ -2,6 +2,7 @@ import pygame
 
 WIDTH = 600
 HEIGHT = 600
+BOX_SIZE = 20
 
 class SnakeGame():
     def __init__(self):
@@ -9,13 +10,15 @@ class SnakeGame():
         self.display = pygame.display.set_mode((WIDTH, HEIGHT))
         pygame.display.set_caption("Snake Game")
 
+        self.columns, self.rows = WIDTH // BOX_SIZE, HEIGHT // BOX_SIZE
+        self.grid = [[0 for _ in range(self.rows)] for _ in range(self.columns)]
         self.snake = []
         self.snake_head = {}
         self.score = 0
         self.current_score = 0 # peut etre recuperer depuis un fichier
         
     def init(self):
-        print("OK")
+        print(self.grid)
 
     def handleEvenement(self):
         pass
@@ -28,6 +31,9 @@ class SnakeGame():
 
     def generate_food(self):
         pass
+
+    def drawGrid(self):
+
 
 
 
