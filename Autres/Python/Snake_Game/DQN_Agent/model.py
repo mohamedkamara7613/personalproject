925c471ff8fa5de82639b843425e73a20fb6784d
mohamedkamara7613 <mohamedkamara7613@gmail.com>
2025-07-19 05:40:03 +0200

    Refactor Linear_QNet class initialization and update model file structure

--- Autres/Python/Snake_Game/DQN_Agent/model.py
+++ Autres/Python/Snake_Game/DQN_Agent/model.py
@@ -1,12 +1,11 @@
 # -*- coding: utf-8 -*-
 import torch
 import torch.nn as nn
-import torch.optim as optim
 import torch.nn.functional as F
 
-class Linear_QNet(nn.Module):
+class Linear_QNet(torch.nn.Module):
     def __init__(self, input_size, hidden_size, output_size):
-        super(Linear_QNet, self).init()
+        super(Linear_QNet, self).__init__()
         self.linear1 = nn.Linear(input_size, hidden_size)
         self.linear2 = nn.Linear(hidden_size, output_size)
 
